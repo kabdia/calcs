15ea8e1f20bfccb32170b76db56176fd5f1d850c
Diana Kabakova <kabdia@mail.ru>
2025-10-05 22:10:27 +0500

    feat: fix calcCredit

--- scripts/calcMath/calcCredit.js
+++ scripts/calcMath/calcCredit.js
@@ -46,6 +46,8 @@ class CalcCredit {
         if (!this.resSum) {
           return
         }
+        
+        this.flagValid = true;
         this.chartInstance = null;
         this.dataForChart = {percent: 13158, currentSum: 300000}
         this.instance.addEventListener('click', this.validationInput.bind(this));
@@ -62,52 +64,54 @@ class CalcCredit {
       const sumMonthPay = this.calcPay();
       const overPay = this.calcOverPay(sumMonthPay);
 
-      const percent = this.roundNum(overPay);
-      const totalSum = this.roundNum(Number(this.sumCurrent.value) + overPay);
+      const percent = this.roundNum(overPay);      
+      const totalSum = this.roundNum(Number(this.sumCurrent.value.replaceAll(' ', '')) + overPay);
       //устанавливаем ежемесячный платеж
       this.monthPay.innerHTML = '';
-      this.monthPay.innerHTML = this.roundNum(sumMonthPay);
-      //устанавливаем переплату
+      this.monthPay.innerHTML = this.divideNumberByPieces(this.roundNum(sumMonthPay));
+      //устанавливаем сумму переплаты
       this.sumOverPay.innerHTML = '';
-      this.sumOverPay.innerHTML = percent;
+      this.sumOverPay.innerHTML = this.divideNumberByPieces(percent);
       //устанавливаем сумму кредита и переплаты
       this.resSum.innerHTML = '';      
-      this.resSum.innerHTML = totalSum;
-      //устанавливаем значения в графике      
-      this.setSumCurrentInChart(percent, this.roundNum(Number(this.sumCurrent.value)));   
+      this.resSum.innerHTML = this.divideNumberByPieces(totalSum); 
+      //устанавливаем значения в графике     
+     
+      this.setSumCurrentInChart(percent, this.roundNum(Number(this.sumCurrent.value.replaceAll(' ', ''))));   
       this.showChart();   
     }
     // Расчет ежемесячного платежа
     calcPay() {      
-      const sumCurrent = parseFloat(this.sumCurrent.value); // Сумма кредита
-      const yearPercent = parseFloat(this.percent.value); // Годовая ставка (%)
+      const sumCurrent = parseFloat(this.roundNum(Number(this.sumCurrent.value.replaceAll(' ', '')))); // Сумма кредита     
+      const yearPercent = parseFloat(this.percent.value); // Годовая ставка (%)      
       const months = parseFloat(this.month.value); // Срок в месяцах
 
       const monthPercent = yearPercent / 12 / 100; // Месячная ставка (делим на 12 и на 100)
-      
+
       const annuityRatio = (monthPercent * Math.pow(1 + monthPercent, months)) / 
                       (Math.pow(1 + monthPercent, months) - 1);
 
       return sumCurrent * annuityRatio;
     }
+
     calcOverPay(sumMonthPay) {
-      const sum = parseFloat(this.sumCurrent.value);
+      const sum = parseFloat(this.roundNum(Number(this.sumCurrent.value.replaceAll(' ', '')))); 
       const months = parseFloat(this.month.value);
+     
       return (sumMonthPay * months) - sum;
     }
     showChart() {
       if (this.chartInstance) {            
-        this.chartInstance.update();
-        console.debug(this.chartInstance.data.datasets[0].data)
+        this.chartInstance.update();        
         return;
       }
       this.chartInstance = new Chart(this.chart, {
             type: 'pie',
             data: {
-              labels: ['Проценты', 'Основной долг'],
+              labels: ['Сумма переплат', 'Основной долг'],
               datasets: [{                
                 data: [this.dataForChart.percent, this.dataForChart.currentSum],
-                borderWidth: 1
+                borderWidth: 1,
               }]
             },
             options: {
@@ -126,28 +130,50 @@ class CalcCredit {
     const value = this.selectRound.value;
     return num.toFixed(value);
   } 
-  validationInput(e){    
-    const input = e.target; 
-    
-    if (input === this.sumCurrent) {
+  validationInput(e){         
+    const input = e.target;   
+   
+    if (input === this.sumCurrent) {        
         const validChars = /^[0-9\s]*$/;      
         const value = input.value;               
-        !validChars.test(value) ? input.nextElementSibling.innerHTML = "Недопустимый символ": input.nextElementSibling.innerHTML = '';
-    } else if (input === this.percent) {
-        const validChars = /^[0-9\s\%]*$/;    
+        !validChars.test(value) ? this.checkValidFieldFalse(input) : this.checkValidFieldTrue(input);        
+      } else if (input === this.percent) {
+        const validChars = /^[0-9\s\%\,\.]*$/;    
         const value = input.value;                  
-        !validChars.test(value) ? input.nextElementSibling.innerHTML = "Недопустимый символ": input.nextElementSibling.innerHTML = '';
+        !validChars.test(value) ? this.checkValidFieldFalse(input) : this.checkValidFieldTrue(input);
     } else if (input === this.month) {
         const validChars = /^[0-9\s]*$/;    
         const value = input.value;                  
-        !validChars.test(value) ? input.nextElementSibling.innerHTML = "Недопустимый символ": input.nextElementSibling.innerHTML = '';
-    }
+        !validChars.test(value) ? this.checkValidFieldFalse(input) : this.checkValidFieldTrue(input);
+    }    
+   
+   this.checkAllFields();
   } 
-  setSumCurrentInChart(currentSum, percent) {
+
+  checkValidFieldFalse(field) {
+    field.nextElementSibling.innerHTML = "Недопустимый символ";
+    this.flagValid = false;
+  }
+  checkValidFieldTrue(field) {
+
+    field.nextElementSibling.innerHTML = '';
+    this.flagValid = true;
+  }
+  checkAllFields() {    
+    const checkArr = [...this.fieldError].filter((field)=> field.innerHTML !== '');
+    checkArr.length !== 0 ? this.button.disabled = true : this.button.disabled = false;
+  }
+  
+  setSumCurrentInChart(currentSum, percent) {    
     this.dataForChart.currentSum = currentSum;
-    this.dataForChart.percent = percent;
+    this.dataForChart.percent = percent;    
+    
     this.chartInstance.data.datasets[0].data = [this.dataForChart.currentSum, this.dataForChart.percent];
-  }           
+  }   
+  //разделитель чисел по разрядам
+  divideNumberByPieces(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}        
 }
 
 
